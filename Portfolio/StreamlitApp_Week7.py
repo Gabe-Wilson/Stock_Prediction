bc37a3e83b7677592200e0266615e9ac13397ba4
Gabe-Wilson <gabewwilson11@gmail.com>
2026-03-26 14:37:24 -0500

    Update StreamlitApp_Week7.py

--- Portfolio/StreamlitApp_Week7.py
+++ Portfolio/StreamlitApp_Week7.py
@@ -25,8 +25,8 @@ warnings.simplefilter("ignore")
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
 if project_root not in sys.path:
-    sys.path.append(project_root)
-
+    sys.path.insert(0, project_root)
+    
 from src.feature_utils import extract_features_pair
 
 # ── Secrets ───────────────────────────────────────────────────────────────────
